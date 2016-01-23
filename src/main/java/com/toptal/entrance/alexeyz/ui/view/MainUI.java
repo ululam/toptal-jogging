7e5313cc29215f859690a70c8154df381a1eea03
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2016-01-23 23:45:01 +0300

    Events removed: no need in a little project

--- src/main/java/com/toptal/entrance/alexeyz/ui/view/MainUI.java
+++ src/main/java/com/toptal/entrance/alexeyz/ui/view/MainUI.java
@@ -1,10 +1,7 @@
 package com.toptal.entrance.alexeyz.ui.view;
 
-import com.google.gwt.thirdparty.guava.common.eventbus.Subscribe;
 import com.toptal.entrance.alexeyz.db.JoggingRepository;
 import com.toptal.entrance.alexeyz.db.UserRepository;
-import com.toptal.entrance.alexeyz.ui.event.ApplicationEventBus;
-import com.toptal.entrance.alexeyz.ui.event.Events;
 import com.vaadin.annotations.PreserveOnRefresh;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
@@ -12,11 +9,10 @@ import com.vaadin.server.Responsive;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.Window;
 import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static com.toptal.entrance.alexeyz.util.UserUtil.*;
+import static com.toptal.entrance.alexeyz.util.UserUtil.isLoggedIn;
 /**
  * @author alexey.zakharchenko@gmail.com
  */
@@ -33,15 +29,8 @@ public class MainUI extends UI {
     @Autowired
     UserRepository userRepository;
 
-    @Autowired
-    ApplicationEventBus bus;
-
-    LoginView loginView;
-    MainView mainView;
-
     @Override
     protected void init(VaadinRequest request) {
-        bus.register(this);
         Responsive.makeResponsive(this);
         addStyleName(ValoTheme.UI_WITH_MENU);
 
@@ -56,19 +45,6 @@ public class MainUI extends UI {
             //getNavigator().navigateTo(getNavigator().getState());
         } else {
             setContent(new LoginView(this));
-        }
-    }
-
-
-    @Subscribe
-    public void userLoggedOut(final Events.UserLoggedOutEvent event) {
-        logOut();
-    }
-
-    @Subscribe
-    public void closeOpenWindows(final Events.CloseOpenWindowsEvent event) {
-        for (Window window : getWindows()) {
-            window.close();
         }
     }
 
